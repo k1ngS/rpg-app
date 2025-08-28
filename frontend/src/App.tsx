117b20c8234b2cf06a69954700f043ed3a8e05d7
k1ngS <marcos.beltrao@outlook.pt>
2025-08-28 15:30:29 -0300

    feat: update dependencies and add new components
    - Added various Radix UI components for improved UI elements.
    - Integrated Sonner for toast notifications.
    - Introduced a new CharacterDetailsPage component for character details routing.
    - Enhanced CSS with Tailwind CSS and custom properties for theming.
    - Updated TypeScript configuration for better path resolution.
    - Configured Vite to resolve aliases for cleaner imports.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import Layout from "./Layout";
 import Characters from "./pages/characters";
 import Quests from "./pages/quests";
+import CharacterDetailsPage from "./pages/characters/CharacterDetailsPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/characters" element={<Characters />} />
+        <Route path="/characters/:id" element={<CharacterDetailsPage />} />
         <Route path="/quests" element={<Quests />} />
         <Route path="/items" element={<div>Item List Component Here</div>} />
       </Route>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter } from "react-router";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>,
 )
